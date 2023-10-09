9a5be3453bb527594a92f92de1e9fc9f608cb41f
UyarMurat <muratferhatuyar@outlook.com>
2023-10-10 00:45:09 +0200

    Update TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -8,7 +8,7 @@ people = int(input("How many people to split the bill?"))
 #print(bill_with_tip)
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
-total_bill = bil + total_tip_amount
+total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
 
